8fb15f6dfc1ef63075dc8bbdb053fb43dff76cbd
brylimz <superuser@brylimz.com>
2022-01-09 19:45:13 +0700

    completed

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+from urllib import response
+from datetime import datetime
 import requests
+
 USERNAME = "brylimz"
 TOKEN = "9384u23423hdsksdfkj"
 GRAPH_ID = "graph1"
@@ -22,7 +25,6 @@ graph_config = {
     "color": "ajisai"
 }
 
-
 headers = {
     "X-USER-TOKEN": TOKEN
 }
@@ -30,4 +32,23 @@ headers = {
 #
 # response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
 # print(response.text)
-pixel_creation_endpoint =  f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
+pixel_creation_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
+print(pixel_creation_endpoint)
+today = datetime.now()
+pixel_data = {
+    "date": today.strftime("%Y%m%d"),
+    "quantity": input("How many kilometeres di you cycle today?"),
+}
+response = requests.post(url=pixel_creation_endpoint, json=pixel_data, headers=headers)
+# # print(response.text)
+
+# update_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
+# new_pixel_data = {
+#     "quantity": "4.5"
+# }
+
+# response = requests.put(url=update_endpoint, json=new_pixel_data, headers=headers)
+# print(response.text)
+
+# delete_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
+# response = requests.delete(url=delete_endpoint, headers=headers)
